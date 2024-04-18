56b524041bbb3dcbb2f9ae137d28cea0220e7574
Umesh Maheswaran <umeshmaheswaran@MacBook-Pro-de-Umesh.local>
2024-04-18 16:31:20 +0200

    favoris page started

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+// import Cookies from "js-cookie";
 
 const Comics = ({ search }) => {
   const [comicsData, setComicsData] = useState({});
@@ -22,6 +23,16 @@ const Comics = ({ search }) => {
     console.log("test");
   }, [search]);
 
+  // const addToFavoris = (Comics) => {
+  //   const favoris = Cookies.get("favoris") || [];
+  //   if (!favoris.includes(Comics)) {
+  //     favoris.push(Comics);
+  //     Cookies.set("favoris", favoris);
+  //     console.log(favoris);
+  //   }
+  // };
+  // addToFavoris();
+
   return isLoading ? (
     <p>Loading...</p>
   ) : (
@@ -30,17 +41,19 @@ const Comics = ({ search }) => {
         <div className="comics-back">
           {comicsData.results.map((comics) => {
             return (
-              <Link key={comics._id} to={`/comic/${comics._id}`}>
-                <article className="comics-all" key={comics._id}>
-                  <h2 className="comics-name">{comics.title}</h2>
-                  <img
-                    className="comics-photo"
-                    src={`${comics.thumbnail.path}/portrait_fantastic.${comics.thumbnail.extension}`}
-                    alt="img-comics"
-                  />
-                  {/* <p className="comics-description">{comics.description}</p> */}
-                </article>
-              </Link>
+              <>
+                <Link key={comics._id} to={`/comic/${comics._id}`}>
+                  <article className="comics-all" key={comics._id}>
+                    <h2 className="comics-name">{comics.title}</h2>
+                    <img
+                      className="comics-photo"
+                      src={`${comics.thumbnail.path}/portrait_fantastic.${comics.thumbnail.extension}`}
+                      alt="img-comics"
+                    />
+                    {/* <p className="comics-description">{comics.description}</p> */}
+                  </article>
+                </Link>{" "}
+              </>
             );
           })}
         </div>
--- src/pages/Favoris.jsx
+++ src/pages/Favoris.jsx
@@ -1,9 +1,7 @@
-import Cookies from "js-cookie";
-import { Navigate, useNaviagte } from "react-router-dom";
+// import Cookies from "js-cookie";
+// import { Navigate, useNaviagte } from "react-router-dom";
 
 const Favoris = () => {
-  const favoris = Cookies.getJSON(`favoris`);
-
   return (
     <>
       <h1>Je suis la page Favoris</h1>
